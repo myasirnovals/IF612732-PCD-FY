5619003d4fd29b82a65934882c4c12b11cb0304d
Juwew1 <fauzyjunior53@gmail.com>
2025-12-04 16:11:09 +0700

    Perbaikan Code

--- MiniPhotoShop/Controllers/FilterController.cs
+++ MiniPhotoShop/Controllers/FilterController.cs
@@ -4,6 +4,7 @@ using MiniPhotoShop.Filters.ColorsFilters;
 using MiniPhotoShop.Managers;
 using MiniPhotoShop.Models;
 using MiniPhotoShop.Services.Interfaces;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -13,13 +14,14 @@ namespace MiniPhotoShop.Controllers
     {
         private readonly DocumentManager _docManager;
         private readonly IImageProcessingService _processor;
+        private Dictionary<TabPage, bool> IsBitwiseDocument = new Dictionary<TabPage, bool>();
 
         public FilterController(DocumentManager docManager, IImageProcessingService processor)
         {
             _docManager = docManager;
             _processor = processor;
         }
-        
+
         public void ApplyFilter(IImageFilter filter, TabPage activeTab)
         {
             ImageDocument doc = _docManager.GetActiveDocument();
@@ -35,8 +37,8 @@ namespace MiniPhotoShop.Controllers
                 }
             }
             bool isBitwise = (filter is NotFilter);
-            
-            if (activeTab != null) 
+
+            if (activeTab != null)
             {
                 if (IsBitwiseDocument.ContainsKey(activeTab))
                     IsBitwiseDocument[activeTab] = isBitwise;
@@ -44,23 +46,24 @@ namespace MiniPhotoShop.Controllers
                     IsBitwiseDocument.Add(activeTab, isBitwise);
             }
 
-            doc.ApplyFilter(filter);
-            _docManager.UpdateActiveCanvas()
+            _docManager.UpdateActiveCanvas();
         }
 
         public void ApplyThreshold(int threshold, TabPage activeTab)
         {
-            ApplyFilter(new ThresholdFilter(threshold), activeTab); 
+            ApplyFilter(new ThresholdFilter(threshold), activeTab);
         }
 
         public void ApplyBrightness(int brightness)
         {
             ApplyFilter(new BrightnessFilter(brightness), null);
+        }
+
         public void ApplyBrightness(int value, TabPage activeTab)
         {
             ApplyFilter(new BrightnessFilter(value), activeTab);
         }
-        
+
         public void RemoveDocument(TabPage tab)
         {
             if (tab != null && IsBitwiseDocument.ContainsKey(tab))
@@ -81,24 +84,24 @@ namespace MiniPhotoShop.Controllers
             switch (filterType)
             {
                 case "Identity":
-                    kernel = new double[,] { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } };
+                    kernel = BaseKernel.Identity;
                     break;
 
                 case "Blur":
-                    kernel = new double[,] { { 1, 2, 1 }, { 2, 4, 2 }, { 1, 2, 1 } };
-                    factor = 1.0 / 16.0;
+                    kernel = BaseKernel.GaussianBlur;
+                    factor = 1.0 / 256.0;
                     break;
 
                 case "Sharpen":
-                    kernel = new double[,] { { 0, -1, 0 }, { -1, 5, -1 }, { 0, -1, 0 } };
+                    kernel = BaseKernel.Sharpen;
                     break;
 
                 case "EdgeDetection":
-                    kernel = new double[,] { { -1, -1, -1 }, { -1, 8, -1 }, { -1, -1, -1 } };
+                    kernel = BaseKernel.EdgeDetection;
                     break;
 
                 case "Emboss":
-                    kernel = new double[,] { { -2, -1, 0 }, { -1, 1, 1 }, { 0, 1, 2 } };
+                    kernel = BaseKernel.Emboss;
                     break;
             }
 
---
+++ MiniPhotoShop/Filters/Base/BaseKernel.cs
@@ -0,0 +1,42 @@
+﻿namespace MiniPhotoShop.Filters.Base
+{
+    public static class BaseKernel
+    {
+        public static double[,] Identity => new double[,]
+        {
+            { 0, 0, 0 },
+            { 0, 1, 0 },
+            { 0, 0, 0 }
+        };
+
+        public static double[,] GaussianBlur => new double[,]
+        {
+            { 1,  4,  6,  4, 1 },
+            { 4, 16, 24, 16, 4 },
+            { 6, 24, 36, 24, 6 },
+            { 4, 16, 24, 16, 4 },
+            { 1,  4,  6,  4, 1 }
+        };
+
+        public static double[,] Sharpen => new double[,]
+        {
+            { 0, -1, 0 },
+            { -1, 5, -1 },
+            { 0, -1, 0 }
+        };
+
+        public static double[,] EdgeDetection => new double[,]
+        {
+            { -1, -1, -1 },
+            { -1, 8, -1 },
+            { -1, -1, -1 }
+        };
+
+        public static double[,] Emboss => new double[,]
+        {
+            { -2, -1, 0 },
+            { -1, 1, 1 },
+            { 0, 1, 2 }
+        };
+    }
+}
