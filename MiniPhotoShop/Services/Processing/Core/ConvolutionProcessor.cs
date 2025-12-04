d41a51580066e494f6a14013882a1d56b1012385
Muhamad Yasir Noval <myasirnoval@gmail.com>
2025-12-04 17:16:23 +0700

    perbaikan perhitungan kernel

--- MiniPhotoShop/Services/Processing/Core/ConvolutionProcessor.cs
+++ MiniPhotoShop/Services/Processing/Core/ConvolutionProcessor.cs
@@ -29,20 +29,41 @@ namespace MiniPhotoShop.Services.Processing.Core
             int radiusX = kernelWidth / 2;
             int radiusY = kernelHeight / 2;
             
-            for (int y = radiusY; y < height - radiusY; y++)
+            double kernelTotal = 0;
+            for (int i = 0; i < kernelWidth; i++)
+            {
+                for (int j = 0; j < kernelHeight; j++)
+                {
+                    kernelTotal += kernel[j, i];
+                }
+            }
+            
+            if (Math.Abs(kernelTotal) < 0.00001) kernelTotal = 1.0;
+            
+            for (int y = 0; y < height; y++)
             {
                 byte* resRow = resPtr + (y * stride);
                 
-                for (int x = radiusX; x < width - radiusX; x++)
+                for (int x = 0; x < width; x++)
                 {
                     double sumR = 0, sumG = 0, sumB = 0;
                     
                     for (int ky = -radiusY; ky <= radiusY; ky++)
                     {
-                        byte* neighborRow = srcPtr + ((y + ky) * stride);
+                        int sampleY = y + ky;
+                        if (sampleY < 0) sampleY = 0;
+                        else if (sampleY >= height) sampleY = height - 1;
+
+                        byte* neighborRow = srcPtr + (sampleY * stride);
+
                         for (int kx = -radiusX; kx <= radiusX; kx++)
                         {
-                            int pixelOffset = (x + kx) * bytesPerPixel;
+                            int sampleX = x + kx;
+                            if (sampleX < 0) sampleX = 0;
+                            else if (sampleX >= width) sampleX = width - 1;
+
+                            int pixelOffset = sampleX * bytesPerPixel;
+                            
                             double weight = kernel[ky + radiusY, kx + radiusX];
 
                             sumB += neighborRow[pixelOffset] * weight;     
@@ -50,6 +71,10 @@ namespace MiniPhotoShop.Services.Processing.Core
                             sumR += neighborRow[pixelOffset + 2] * weight; 
                         }
                     }
+                    
+                    sumB /= kernelTotal;
+                    sumG /= kernelTotal;
+                    sumR /= kernelTotal;
                     
                     int resOffset = x * bytesPerPixel;
                     resRow[resOffset] = (byte)Math.Clamp(sumB, 0, 255);     
